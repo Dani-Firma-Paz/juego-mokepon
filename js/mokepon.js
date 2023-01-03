f13b0beca4973cf8295f9a03b05645f41d97466a
Daniela Firma Paz <danii.firma.paz@gmail.com>
2023-01-03 14:29:23 -0300

    first commit

---
+++ js/mokepon.js
@@ -0,0 +1,11 @@
+alert("Hola, mundo JS");
+
+function seleccionarMascotaJugador() {
+    alert("SELECCIONASTE TU MASCOTA")
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota');
+
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
+
