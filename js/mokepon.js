c28ea3b65092bdd174d9ebd74f6c4cd13af2d0a2
Daniela Firma Paz <danii.firma.paz@gmail.com>
2023-01-07 19:10:15 -0300

    third commit

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,13 +1,46 @@
-alert("Hola, mundo JS");
+alert("Bienvenidos a Mokepon");
 
-function () {
+function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota');
 
 botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
 
 function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE TU MASCOTA")
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else {
+        alert('Debes seleccionar una mascota')
+    }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+
+}
+
+function aleatorio(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 
